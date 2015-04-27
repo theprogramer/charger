67c738d5c32fff95bde82d8be829347006493750
Thiago Miranda <theprogramer@theprogramer.com.br>
2015-04-27 11:35:53 -0300

    Append migrations

--- lib/charger/engine.rb
+++ lib/charger/engine.rb
@@ -7,5 +7,14 @@ module Charger
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
+
+    # Append Charger migrations 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
