37ba0a65fd666c410baf1d3419037b868e011c30
Thiago Miranda <theprogramer@theprogramer.com.br>
2015-04-27 13:00:21 -0300

    Config ActiveAdmin on dummy app

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   mount Charger::Engine => "/charger"
 end
