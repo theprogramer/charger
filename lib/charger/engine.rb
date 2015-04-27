d1621002e94e74313ec7ce1309a8dae629f853f3
Thiago Miranda <theprogramer@theprogramer.com.br>
2015-04-27 11:25:19 -0300

    Config Rspec

--- lib/charger/engine.rb
+++ lib/charger/engine.rb
@@ -1,5 +1,11 @@
 module Charger
   class Engine < ::Rails::Engine
     isolate_namespace Charger
+
+    # Use Rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
