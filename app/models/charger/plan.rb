3ea6600527aeff6dcfa00372f50300df4f1a3a68
Thiago Miranda <theprogramer@theprogramer.com.br>
2015-04-27 12:36:30 -0300

    Generate Plan migration

---
+++ app/models/charger/plan.rb
@@ -0,0 +1,4 @@
+module Charger
+  class Plan < ActiveRecord::Base
+  end
+end
---
+++ db/migrate/20150427145253_create_charger_plans.rb
@@ -0,0 +1,22 @@
+class CreateChargerPlans < ActiveRecord::Migration
+  def change
+    create_table :charger_plans do |t|
+      t.string :name, null: false
+      t.string :slug, default: ''
+      t.text :description
+      t.decimal :amount, :decimal, precision: 8, scale: 2, default: 0
+      t.integer :interval_length, default: 1
+      t.integer :interval_unit, default: 0
+      t.decimal :setup_fee, :decimal, precision: 8, scale: 2, default: 0
+      t.boolean :renew, default: true
+      t.integer :billing_cycles, default: 0
+      t.integer :trial_days, default: 0
+      t.boolean :hold_setup_fee, default: false
+      t.boolean :enabled, default: true
+      # t.boolean :max
+      # t.integer :max_qty
+
+      t.timestamps null: false
+    end
+  end
+end
